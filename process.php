6e20346a785a8b3bd7e37edb3b0fa6a335405694
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-09-20 14:06:40 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,3 +1,13 @@
 <?php
-  echo $_GET['data'];
+  
+  $username =  $_GET['data1'];
+  $password =  $_GET['data2'];
+
+  if($username == "admin" && $password == "1234"){
+    echo "ยินดีต้อนรับเข้าสู่ระบบ";
+  }
+  else {
+    echo "ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด";
+  }
+
 ?>

e95d25d66ef35ada5ca871aacec77b2e539a296f
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-11-19 14:30:32 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -2,10 +2,15 @@
   <head>
   </head>
   <body>
-    <table border="6">
+    <table border="1">
       <?php
         for($i=0;$i<100;$i++){
           echo "<tr>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
           echo "<td>คอลัมน์</td>";
           echo "</tr>";
         }

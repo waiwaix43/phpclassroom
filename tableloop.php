c6fcbbfd7abbe9ba1850a7b233115efbdfce629c
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-11-19 14:10:33 +0700

    Create tableloop.php

---
+++ tableloop.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <table border="1">
+      <?php
+        for($i=0;$i<5;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>

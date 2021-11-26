9e1d5877a54fdfd79f19416fc974bb69f219f214
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-11-26 14:37:49 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,8 +1,26 @@
 <?php
   $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
   $data = json_decode($json);
-
-  echo "<table border='1'>";
+<html>
+  <head>
+  </head>
+  <body>
+    <table border="0">
+      <?php
+        for($i=0;$i<100;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "<td>คอลัมน์</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
 
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";

63dc8cb91c191ee381be5ff4d554d365ea0ce67e
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-11-26 14:58:52 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -2,7 +2,7 @@
  $json = file_get_contents(' https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
  $data = json_decode($json);
 
- echo <table border="0">;
+ echo <table border="1">;
 
  foreach ( $data[0] as $key => $val ){
  echo $key.":".$val."<br>";

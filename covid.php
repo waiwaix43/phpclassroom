37ced6992990b13712b84aaf151df180ec3305b8
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-11-26 15:56:24 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -7,8 +7,10 @@
  foreach ( $data[0] as $key => $val ){
     echo "<tr>";
     echo "<td>";
+    echo "<td>";
     echo $key.":".$val."<br>";
     echo "</tr>";
+    echo "</td>";
     echo "</td>";
   }
 

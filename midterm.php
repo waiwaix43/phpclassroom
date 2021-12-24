c949d7e03172c1a74f3a5fe937266c9c312bcd8f
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2021-12-24 20:01:56 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,7 +1,7 @@
 <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-
+<center>
   echo "<table border='1'>";
   echo"<caption>";
   echo "<h3>";
@@ -39,7 +39,7 @@
   echo "วันที่อัพเดท";
   echo "</td>";  
   echo "</tr>";
-
+</center>
   foreach($data as $key=>$val){
     echo "<tr align = 'center'>";
     echo "<tr align = 'center'>";

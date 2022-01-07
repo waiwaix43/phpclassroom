e5841fe0a00d3ae440768184ef26c4180da45275
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-01-07 13:59:45 +0700

    Create page.php

---
+++ page.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+     <style>
+       
+body {
+  background-color: lightblue;
+}
+#box1{
+  width: 500px;
+  height: 500px;
+  ackground-color: red;
+       }
+.box2{
+  width: 500px;
+  height: 500px;
+  background-color: blue;
+       }       
+    </style>
+  </head>
+  <body>
+     <div id="box1">
+     </div>  
+     <div class="box2">
+     </div>  
+
+  </body>
+</html>  

c1f8b8ae76c298e5b93b97314b0c297b160827ef
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-01-07 14:16:30 +0700

    Update page.php

--- page.php
+++ page.php
@@ -1,27 +1,45 @@
-<html>
+<!doctype html>
+<html lang="en">
   <head>
-     <style>
-       
-body {
-  background-color: lightblue;
-}
-#box1{
-  width: 500px;
-  height: 500px;
-  background-color: red;
-       }
-.box2{
-  width: 500px;
-  height: 500px;
-  background-color: blue;
-       }       
-    </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Hello, world!</title>]
+    
+    <style>
+      #box1{
+        height: 50px;
+        background-color: red;
+      }
+     #box2{
+        height: 50px;
+        background-color: green;
+      }
+      #box3{
+        height: 50px;
+        background-color: blue;
+      }
+    </style>  
+    
   </head>
   <body>
-     <div id="box1">
-     </div>  
-     <div class="box2">
-     </div>  
-
+    
+    <div class="container">
+      <div class="row">
+        <div class="col" id="box1">
+          Column
+        </div>
+        <div class="col" id="box2">
+          Column
+        </div>
+        <div class="col" id="box3">
+          Column
+        </div>
+      </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
-</html>  
+</html>

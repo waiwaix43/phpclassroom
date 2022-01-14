1d3c17e45ecb454e452ab72749b45d4c8c144b60
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-01-14 13:52:49 +0700

    Create gifted.php

---
+++ gifted.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title> G I F T E D </title>
+  <style>
+    h1{
+      color:blue;
+      
+  </style>
+</head>
+<body>
+  <h1 style="color:red;"> ยินดีต้อนรับสู่ห้อง GIFTED </h1>
+</body>
+</html>

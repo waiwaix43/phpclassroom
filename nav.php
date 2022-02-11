31ffafc44c9894c2514725c2bb5cf0d6e68eaf07
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-11 14:02:54 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,6 +1,20 @@
 <html>
   <head>
     <title>Navigation Bar</title>
+    <style>
+      ul {
+        list-style-type: none;
+      }
+      li a {
+        display: block;
+        padding: 8px 16px;
+        text-decoration: none;
+      }
+      li a:hover {
+        background-color: #555;
+        color: white;
+      }
+    </style>
   </head>
   <body>
     <ul>
